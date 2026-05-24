b378d99447906828161bbea0d5421a31362ab18b
shamsuttabriz <shamsuttabriz.md@gmail.com>
2026-05-24 11:08:53 +0600

    Update database connection string and add peer dependency for pg

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
   connectionString:
-    "postgresql://neondb_owner:npg_Fnj89PkvLWsr@ep-blue-scene-aok1kgi0-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    "postgresql://neondb_owner:npg_WrzN6AGco1CQ@ep-long-smoke-ao1s2nkh-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
 const initDB = async () => {
@@ -38,5 +38,18 @@ initDB();
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Hello World!" });
 });
+
+app.post("/", (req: Request, res: Response) => {
+  const {name, email, password, role} = req.body;
+  res.status(200).json({
+    message: "User created successfully!",
+    user: {
+      name,
+      email,
+      password,
+      role
+    }
+  });
+})
 
 export default app;
