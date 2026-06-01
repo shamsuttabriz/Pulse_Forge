c03cf19c346a04cb6d98a070df91bcceb17481a3
shamsuttabriz <shamsuttabriz.md@gmail.com>
2026-06-01 13:26:22 +0600

    Add bcryptjs for password hashing in user service

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,49 +1,69 @@
+import bcrypt from "bcryptjs";
 import { pool } from "../../db";
 import type { IUser } from "./user.interface";
 
 const createUserIntoDb = async (payload: IUser) => {
-  const { name, email, password, role = 'contributor' } = payload;
+  const { name, email, password, role = "contributor" } = payload;
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
   const result = await pool.query(
     `
     INSERT INTO users (name, email, password, role)
     VALUES ($1, $2, $3, $4)
     RETURNING *
   `,
-    [name, email, password, role],
+    [name, email, hashPassword, role],
   );
+
+  delete result.rows[0].password;
 
   return result;
 };
 
 const getAllUsersFromDb = async () => {
   const result = await pool.query("SELECT * FROM users");
+  result.rows.forEach((user) => delete user.password);
   return result;
 };
 
 const getUserByIdFromDb = async (id: string) => {
-    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-    return result;
-}
+  const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+  if (result.rows.length === 0) {
+    return null;
+  }
+  delete result.rows[0].password;
+  return result;
+};
 
 const updateUserInDb = async (id: string, payload: Partial<IUser>) => {
-    const { name, password, role } = payload;
-    const result = await pool.query(
-      `
+  const { name, password, role } = payload;
+  const hashPassword = password ? await bcrypt.hash(password, 10) : undefined;
+  const result = await pool.query(
+    `
         UPDATE users SET name = COALESCE($1, name), password = COALESCE($2, password), role = COALESCE($3, role)
         WHERE id = $4 RETURNING *
       `,
-      [name, password, role, id],
-    );
-    return result;
-}
+    [name, hashPassword, role, id],
+  );
+  if (result.rows.length === 0) {
+    return null;
+  }
+  delete result.rows[0].password;
+  return result;
+};
 
 const deleteUserFromDb = async (id: string) => {
-    const result = await pool.query(
-      `DELETE FROM users WHERE id = $1 RETURNING *`,
-      [id],
-    );
-    return result;
-}
+  const result = await pool.query(
+    `DELETE FROM users WHERE id = $1 RETURNING *`,
+    [id],
+  );
+  if (result.rows.length === 0) {
+    return null;
+  }
+  delete result.rows[0].password;
+  return result;
+};
 
 export const userService = {
   createUserIntoDb,
